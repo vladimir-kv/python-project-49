99baf7aacc561a2422a54fa3e02c60b9fee176c5
vladimir-kv <koblov.vladimir@gmail.com>
2025-02-07 20:14:32 +0400

    task_1

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,9 @@
+def main():
+    print("Welcome to the Brain Games!")
+
+
+if __name__ == '__main__':
+    main()
+
+#Запускается командой 'uv run python -m brain_games.scripts.brain_games' из консоли
+#Или 'uv run brain-games'
